c5bdbb0abf14288f30f43a47abac93f4931d4018
shilpa <shilpa07udupi@gmail.com>
2020-09-05 20:59:35 +0530

    fliping till head or reaches 21 times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,34 +1,34 @@
 echo welcome to flip coin simulation problem !
 
-randomNum=$((RANDOM%2))
-
-if [[ $randomNum -eq 0 ]]
-then
-        echo HEAD
-else
-        echo TAIL
-fi
-
-echo multiple time flipping coin
-
 i=0
 headCount=0
 tailCount=0
 
-while [[ $i -lt 20 ]]
+while [[ $headCount -lt 21 ]] && [[ $tailCount -lt 21 ]]
 do
 randomNum=$((RANDOM%2))
 
 if [[ $randomNum -eq 0 ]]
 then
-	echo HEAD
 	headCount=$((headCount+1))
 else
-	echo TAIL
 	tailCount=$((tailCount+1))
 fi
 i=$(($i+1))
 done
 
-echo head won $headCount times
-echo tail won $tailCount times
+echo "head :"$headCount 
+echo "tail :" $tailCount 
+
+if [ $headCount -eq 21 ]
+then
+	wonBy=$(($headCount-$tailCount))
+	echo "head won by:" $wonBy
+elif [ $tailCount -eq 21 ]
+then
+	wonBy=$(($tailCount-$headCount))
+	echo "tail won by:" $wonBy
+elif [ $headCount -eq $tailCount ]
+then
+	echo "tie"
+fi
