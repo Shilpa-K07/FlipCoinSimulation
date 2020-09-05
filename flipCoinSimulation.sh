74a6ab1bcb3b52e9a0a8d20ede0e9a31175cfc12
shilpa <shilpa07udupi@gmail.com>
2020-09-05 22:01:44 +0530

    flipping continues if it is tie

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,4 +1,38 @@
 echo welcome to flip coin simulation problem !
+randomNum=$((RANDOM%2))
+
+if [[ $randomNum -eq 0 ]]
+then
+        echo HEAD
+else
+        echo TAIL
+fi
+
+echo multiple time flipping coin
+
+i=0
+headCount=0
+tailCount=0
+
+while [[ $i -lt 20 ]]
+do
+randomNum=$((RANDOM%2))
+
+if [[ $randomNum -eq 0 ]]
+then
+	echo HEAD
+	headCount=$((headCount+1))
+else
+	echo TAIL
+	tailCount=$((tailCount+1))
+fi
+i=$(($i+1))
+done
+
+echo head won $headCount times
+echo tail won $tailCount times
+
+echo flipping till head or tail reaches to 21
 
 i=0
 headCount=0
@@ -32,7 +66,6 @@ elif [ $headCount -eq $tailCount ]
 then
 	echo "tie"
 fi
-
 
 function resolvingTie
 {
@@ -73,6 +106,7 @@ while [ $i -eq 0 ]
  echo "tail:" $tailCount
 
 }
+
 if [ $headCount -eq $tailCount ]
 then
 	echo continuing till difference of 2 is achieved
